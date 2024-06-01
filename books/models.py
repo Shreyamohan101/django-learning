9f8372f00986e539b0c910fc506a2f5a6b839acd
Shreya Mohan <shreyamohan.2nov@gmail.com>
2024-06-01 23:43:56 +0530

    some changes in models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    rollno = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    Book_issued = models.CharField(max_length=100)
+    Date_issued = models.DateField()
+    Date_return = models.DateField(null = True)
